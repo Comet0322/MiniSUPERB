1d35102be0364c1b069c576e99af013861d334b6
Jerry <Comet0322@protonmail.com>
2024-01-07 15:13:13 +0800

    Remove unused code and functions

--- minisuperb/upstream/utils.py
+++ minisuperb/upstream/utils.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass, is_dataclass
 import torch
 
 from minisuperb.util.download import _urls_to_filepaths
-from minisuperb.util.pseudo_data import get_pseudo_wavs
 
 logger = logging.getLogger(__name__)
 
@@ -44,21 +43,3 @@ def merge_with_parent(dc: dataclass, cfg: dict):
     assert len(cfg) > 0
     return dc(**cfg)
 
-
-def extract_hidden_states(model):
-    model.eval()
-    with torch.no_grad():
-        return model(get_pseudo_wavs())["hidden_states"]
-
-
-def are_same_models(model1, model2):
-    hs1 = extract_hidden_states(model1)
-    hs2 = extract_hidden_states(model2)
-    for h1, h2 in zip(hs1, hs2):
-        assert torch.allclose(h1, h2)
-
-
-def models_all_close(*models):
-    assert len(models) > 1
-    for model in models[1:]:
-        are_same_models(models[0], model)
